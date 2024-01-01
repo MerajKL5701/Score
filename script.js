d4de6e5ce3b9817b81faca6f43998bce895ec2ce
MerajKL5701 <137307251+MerajKL5701@users.noreply.github.com>
2024-01-01 09:38:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function customMinus()
     let today = Math.floor((Date.now() + ((hour)* 4)) / day);// today duh
     console.log(x +  "today" + today - 19709)
 
-    if (x == 6 || today - 19709 >= 30 || x!= 6)
+    if (x == 6 || today - 19709 >= 30 )
     {
         prize.style.display = "block"
         return;
@@ -81,8 +81,8 @@ function CalcMinus()
     
     let n = parseInt(score.innerHTML);
     let c = minusprompt.value;
-    const Tpoints = 76;
-    // const Tpoints = localStorage.getItem('cachedTpoints');
+
+     const Tpoints = localStorage.getItem('cachedTpoints');
     
     if (n < c)
     {
